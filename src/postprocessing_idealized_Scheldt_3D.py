8fe455f27feb267131a9c3033f55727cc4faa80a
Qilong <qilong.bi@gmail.com>
2019-08-12 22:48:08 +0200

    Update postprocessing_idealized_Scheldt_3D.py

--- src/postprocessing_idealized_Scheldt_3D.py
+++ src/postprocessing_idealized_Scheldt_3D.py
@@ -104,57 +104,95 @@ class Reading_TelemacData3D:
         return node_thalweg3D, X_thalweg, Z_thalweg, varPlane
 
 # test
-slf_file = 'C:\\Users\\saaad264\\Research\\MCPBE_flocculation_model\\idealized_Scheldt\\r3D_idealized_Scheldt_2CPBE.slf'
+slf_file = 'D:\\QILONG_BI\\Research\\MCPBE_flocculation_model\\idealized_Scheldt\\r3D_idealized_Scheldt_2CPBE.slf'
 r3d = Reading_TelemacData3D(slf_file)
 
-IPOIN2D = 6539
-start_t = 200
-end_t = 260
+IPOIN2D = 5550
+start_t = 355
+end_t = 429
 
 IPOIN3D, raw_profiles = r3d.get_raw_profile(IPOIN2D)
-varProfile = r3d.get_var_profile(raw_profiles, 10, IPOIN3D, start_t, end_t)
+varProfile = r3d.get_var_profile(raw_profiles, 4, IPOIN3D, start_t, end_t)
 
 #--------------------------------------
-timestep = 252
-for timestep in range(200,260):
-    node_thalweg3D, X_thalweg, Z_thalweg, varPlane = r3d.get_2DV_slice_thalweg(timestep,10,100)
+timestep = 373
+for timestep in range(365,381):
+    node_thalweg3D, X_thalweg, Z_thalweg, varPlane = r3d.get_2DV_slice_thalweg(timestep,6,100)
 
 #--------------------------------------
 # computing settling flux
-slf_file = 'C:\\Users\\saaad264\\Research\\MCPBE_flocculation_model\\idealized_Scheldt\\r3D_idealized_Scheldt.slf'
-r3d = Reading_TelemacData3D(slf_file)
 
-timestep = 265
+t0 = 355
+t1 = 429
+
+slf_file = 'D:\\QILONG_BI\\Research\\MCPBE_flocculation_model\\idealized_Scheldt\\r3D_idealized_Scheldt_2CPBE.slf'
+r3d = Reading_TelemacData3D(slf_file)
 r3d.varNames
-r3d.slf.readVariables(timestep-1)
-varPlane = np.array(r3d.slf.getVarValues())
 # find thalweg points
 node_thalweg3D = [i for i in range(len(r3d.Y)) if r3d.Y[i]==0]
 X_thalweg = np.array([r3d.X[i] for i in range(len(r3d.X)) if r3d.Y[i]==0])
-Z_thalweg = varPlane[r3d.id_z,node_thalweg3D]
+#Z_thalweg = varPlane[r3d.id_z,node_thalweg3D]
+varPlane_total = np.zeros([r3d.slf.NBV1,r3d.slf.NPOIN])
+SettlingFlux_2CPBE = np.zeros([len(node_thalweg3D)])
+for timestep in range(t0,t1+1):
+    r3d.slf.readVariables(timestep)
+    varPlane = np.array(r3d.slf.getVarValues())
+    varPlane_total = varPlane_total+varPlane
+    # C = np.array(varPlane[6,node_thalweg3D])
+    # C = np.array((varPlane[8,node_thalweg3D]+varPlane[10,node_thalweg3D])*varPlane[6,node_thalweg3D])
+    # C = np.array(varPlane[8,node_thalweg3D]+varPlane[10,node_thalweg3D])
+    C = np.multiply((varPlane[8,node_thalweg3D]+varPlane[10,node_thalweg3D]),varPlane[1,node_thalweg3D])
+    SettlingFlux_2CPBE = SettlingFlux_2CPBE+C
+varPlane_total = varPlane_total/(t1-t0+1)
+SettlingFlux_2CPBE = SettlingFlux_2CPBE/(t1-t0+1)
+Z_thalweg = varPlane_total[r3d.id_z,node_thalweg3D]
 
-C1 = (varPlane[8,node_thalweg3D]+varPlane[10,node_thalweg3D])*varPlane[6,node_thalweg3D]
-C2 = (varPlane[8,node_thalweg3D])*1.1006341463414635E-003
+
+slf_file = 'D:\\QILONG_BI\\Research\\MCPBE_flocculation_model\\idealized_Scheldt\\r3D_idealized_Scheldt.slf'
+r3d = Reading_TelemacData3D(slf_file)
+r3d.varNames
+# find thalweg points
+node_thalweg3D = [i for i in range(len(r3d.Y)) if r3d.Y[i]==0]
+X_thalweg = np.array([r3d.X[i] for i in range(len(r3d.X)) if r3d.Y[i]==0])
+#Z_thalweg = varPlane[r3d.id_z,node_thalweg3D]
+varPlane_total = np.zeros([r3d.slf.NBV1,r3d.slf.NPOIN])
+SettlingFlux = np.zeros([len(node_thalweg3D)])
+for timestep in range(t0,t1+1):
+    r3d.slf.readVariables(timestep)
+    varPlane = np.array(r3d.slf.getVarValues())
+    varPlane_total = varPlane_total+varPlane
+    C = np.array((varPlane[8,node_thalweg3D])*0.0037)
+    # C =  np.array(varPlane[8,node_thalweg3D])
+    # C = np.multiply(varPlane[8,node_thalweg3D],varPlane[1,node_thalweg3D])
+    SettlingFlux = SettlingFlux+C
+varPlane_total = varPlane_total/(t1-t0+1)
+SettlingFlux = SettlingFlux/(t1-t0+1)
+Z_thalweg = varPlane_total[r3d.id_z,node_thalweg3D]
+
 
 # triangulation
 triang = tri.Triangulation(X_thalweg, Z_thalweg)
 # mask the outside elements
 node_thalweg_bottom =  [i for i in range(len(r3d.Y)) if (r3d.Y[i]==0 and i<=r3d.NPOIN2D)]
 node_thalweg_surface = [i for i in range(len(r3d.Y)) if (r3d.Y[i]==0 and i>r3d.NPOIN2D*(r3d.NPLAN-1))]
-f_bottom  = interpolate.interp1d(r3d.X[node_thalweg_bottom],  varPlane[r3d.id_z,node_thalweg_bottom])
-f_surface = interpolate.interp1d(r3d.X[node_thalweg_surface], varPlane[r3d.id_z,node_thalweg_surface])
+f_bottom  = interpolate.interp1d(r3d.X[node_thalweg_bottom],  varPlane_total[r3d.id_z,node_thalweg_bottom])
+f_surface = interpolate.interp1d(r3d.X[node_thalweg_surface], varPlane_total[r3d.id_z,node_thalweg_surface])
 triang.set_mask([(Z_thalweg[t].mean() < f_bottom (X_thalweg[t].mean()))
                 or (Z_thalweg[t].mean() > f_surface(X_thalweg[t].mean())) for t in triang.triangles])
 # plot the figure
-plt.tricontourf(triang, C1-C2, levels=200, cmap='coolwarm')
+plt.tricontourf(triang, SettlingFlux_2CPBE-SettlingFlux, levels=200, cmap='coolwarm')
 plt.xlim(5000,155000)
-plt.clim(-0.0003,0.0003)
+# plt.clim(0.0000,0.3)
+plt.clim(-0.02,0.02)
+# plt.clim(80,180)
 cbar = plt.colorbar()
 plt.xlabel('Distance from estuary mouth (km)')
 plt.ylabel('Elevation (m MSL)')
-plt.title('Difference of the settling flux during slack tide')
+plt.title('Difference of settling flux (tidally averaged)')
 cbar.ax.set_ylabel(r'$kg/m^2/s$')
 #plt.triplot(triang, 'ko-')
 #plt.plot(r3d.X[node_thalweg_bottom],  varPlane[r3d.id_z,node_thalweg_bottom],  'ro')
 #plt.plot(r3d.X[node_thalweg_surface], varPlane[r3d.id_z,node_thalweg_surface], 'bo')
 plt.show()
+
+r3d.slf.close()
